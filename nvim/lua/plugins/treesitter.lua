c28880ce5dd01ad71801c201e02bcffa785c6483
Manick Vennimalai <manick.vennimalai@gmail.com>
2025-05-22 22:34:08 +0100

    Updated incremental selection for treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -24,6 +24,15 @@ return {
 				sync_install = false,
 				highlight = { enable = true },
 				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<Enter>",
+						node_incremental = "<Enter>",
+						scope_incremental = false,
+						node_decremental = "<Backspace>",
+					},
+				},
 			})
 		end,
 	},
