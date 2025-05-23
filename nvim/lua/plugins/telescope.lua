46da18503d7e4248e88a8f10ff7a05eccb707a38
Manick Vennimalai <manick.vennimalai@gmail.com>
2025-05-23 21:50:21 +0100

    Removed fzf-lua definition in telescope file as it's now standalone

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,13 +1,4 @@
 return {
-	{
-		"ibhagwan/fzf-lua",
-		-- optional for icon support
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			-- calling `setup` is optional for customization
-			require("fzf-lua").setup({})
-		end,
-	},
 	-- Base Telescope Plugin --
 	{
 		"nvim-telescope/telescope.nvim",
