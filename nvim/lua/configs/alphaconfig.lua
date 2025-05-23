d9d403df4530e22d77336d55ffdd3224064b294b
Manick Vennimalai <manick.vennimalai@gmail.com>
2025-05-23 21:52:05 +0100

    Updated alpha.nvim dash with revised buttons

--- nvim/lua/configs/alphaconfig.lua
+++ nvim/lua/configs/alphaconfig.lua
@@ -26,5 +26,12 @@ M.alpha_dashboard.section.header.val = {
 	"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡆⢰⠋⠀⠀⠀⠉⠉⠉⠀⠀⠈⠑⢳⠋⠀⠀",
 	"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⡤⠎⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
 }
+M.alpha_dashboard.section.buttons.val = {
+	M.alpha_dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	M.alpha_dashboard.button("f", "󰈞  Find Files", ":lua require('fzf-lua').files()<CR>"),
+	M.alpha_dashboard.button("g", "󰊄  Grep Files", ":lua require('fzf-lua').live_grep()<CR>"),
+	M.alpha_dashboard.button("d", "󰈬 Browse Docs", ":lua require('fzf-lua').help_tags()<CR>"),
+	M.alpha_dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+}
 
 return M
