df4ed0f5271f8c5b148bae5ee793aa93bea5a691
Manick Vennimalai <manick.vennimalai@gmail.com>
2025-05-23 23:13:06 +0100

    Added dressing as standalone plugin. Removed telescope-ui-select

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,19 +16,19 @@ return {
 		end,
 	},
 	-- UI Select for Code Actions --
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+	-- {
+	-- 	"nvim-telescope/telescope-ui-select.nvim",
+	-- 	config = function()
+	-- 		require("telescope").setup({
+	-- 			extensions = {
+	-- 				["ui-select"] = {
+	-- 					require("telescope.themes").get_dropdown({}),
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 		require("telescope").load_extension("ui-select")
+	-- 	end,
+	-- },
 	-- Recent Files --
 	{
 		"smartpde/telescope-recent-files",
